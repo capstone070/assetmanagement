1566b0c159a854e529368ae0bf2f1cf90304d633
capstone070 <capstone070@gmail.com>
2023-04-13 14:23:24 -0400

    Make image fluid

--- about/index.php
+++ about/index.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../lib/header.php';
 <div class="container fs-5">
     <h1 class="display-1">About</h1>
 	
-	<p><img src="pix.jpg" alt="group pic with Henry" width="700" height="500"></p>
+	<p><img src="pix.jpg" alt="group pic with Henry" width="700" height="500" class="img-fluid"></p>
     <p>Welcome to Ana's Bad Boyz capstone project website! Our team, consisting of talented individuals, has put together an outstanding project as part of our academic journey. With dedication and hard work, we have successfully completed this capstone project to showcase our skills and knowledge in our respective fields.</p>
 
     <p>Our project, which can be found through the attached <a href="members.php">PDF link</a>, is a culmination of our extensive research, planning, and implementation. As a team, we have collaborated effectively to create a project that we are truly proud of.</p>
